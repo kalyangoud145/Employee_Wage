932721449bedc67a09352aa9bb1e4c56b1eaf578
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-05 16:49:34 +0530

    maximum work hrs =100 and maximum days =20

--- empwageproblem.sh
+++ empwageproblem.sh
@@ -1,26 +1,29 @@
 #!/bin/bash -x
 echo "welcome to employee wage calculation problem"
-isFullTime=2;
-isPartTime=1;
-totalSalary=0;
-empRatePerHr=20;
-numWorkingDays=20;
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-for (( day=1 ; $day <= $numWorkingDays ; day++ ))
+totalEmpHr=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
+   ((totalWorkingDays++))
    empCheck=$((RANDOM%3));
-         case $empCheck in
-            $isPartTime)
-               empHrs=4;
-               ;;
-            $isFullTime)
-               empHrs=8;
-               ;;
-            *)
-               empHrs=0
-               ;;
-         esac
-         salary=$(($empHrs*$empRatePerHr));
-         totalsalary=$(($totalsalary+$salary));
+   case $empCheck in
+      $IS_FULL_TIME)
+         empHrs=8
+         ;;
+      $IS_PART_TIME)
+         empHrs=4
+         ;;
+      *)
+         empHrs=0
+         ;;
+   esac
+   totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
-echo "$totalsalary"
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR));
