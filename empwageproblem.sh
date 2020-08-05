44c6c1a643742f4aab1233a26ca5522da898b1c1
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-05 16:15:20 +0530

    calculating the dailywage wagePerHr =20 hrsPerday=8

--- empwageproblem.sh
+++ empwageproblem.sh
@@ -5,6 +5,12 @@ randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ]
 then
    echo "Employee is present"
+	empRatePerHr=20;
+   empHrs=8;
+   salary=$(($empRatePerHr*$empHrs));
+	echo "$salary"
 else
    echo "Employee is absent"
+	salary=0
+	echo "$salary"
 fi
