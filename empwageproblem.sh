f86f97d8ea5e84a1cae99425b2b2a311407084f8
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-05 16:42:59 +0530

    wage for a month i.e. 20 days

--- empwageproblem.sh
+++ empwageproblem.sh
@@ -1,19 +1,26 @@
 #!/bin/bash -x
 echo "welcome to employee wage calculation problem"
-isPartTime=1;
 isFullTime=2;
+isPartTime=1;
+totalSalary=0;
 empRatePerHr=20;
-empCheck=$((RANDOM%3));
-case $empCheck in
-   $isFullTime)
-      empHrs=8
-      ;;
-   $isPartTime)
-      empHrs=4
-      ;;
-   *)
-      empHrs=0
-      ;;
-esac
-salary=$(($empHrs*$empRatePerHr))
-echo "$salary"
+numWorkingDays=20;
+
+for (( day=1 ; $day <= $numWorkingDays ; day++ ))
+do
+   empCheck=$((RANDOM%3));
+         case $empCheck in
+            $isPartTime)
+               empHrs=4;
+               ;;
+            $isFullTime)
+               empHrs=8;
+               ;;
+            *)
+               empHrs=0
+               ;;
+         esac
+         salary=$(($empHrs*$empRatePerHr));
+         totalsalary=$(($totalsalary+$salary));
+done
+echo "$totalsalary"
